3a175440f4520c2d9a1e8970001a7ebf47f3c5d9
gejon <gejon@DESKTOP-1ILBTPR>
2024-12-17 15:07:16 +0900

    24.12.17 roomCom

---
+++ src/kr/co/na/JavaEduHome/IntegerVariable.java
@@ -0,0 +1,28 @@
+package kr.co.na.JavaEduHome;
+
+public class IntegerVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte bNum = 10;
+		int iNum = bNum;
+		
+		System.out.println(bNum);
+		System.out.println(iNum);
+		
+		int iNum2 = 20;
+		float fNum = iNum2;
+		
+		System.out.println(iNum);
+		System.out.println(fNum);
+		
+		double dNum;
+		dNum = fNum + iNum;
+		System.out.println(dNum);
+		
+		
+		
+	}
+
+}
